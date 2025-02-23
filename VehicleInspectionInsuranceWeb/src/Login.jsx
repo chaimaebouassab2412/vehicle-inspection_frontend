32a51bf1b5a30383569539ed88a6386e5a662eb9
Chaimae Bouassab <140265140+ChaimaeBouassab@users.noreply.github.com>
2025-02-23 21:35:48 +0000

    mise a jour

--- VehicleInspectionInsuranceWeb/src/Login.jsx
+++ VehicleInspectionInsuranceWeb/src/Login.jsx
@@ -1,65 +1,88 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:2018/api/utilisateurs/login', {
+                email,
+                motDePasse: password
+            });
+
+            // Store the JWT Token
+            localStorage.setItem('token', response.data.token);
+            
+            // Redirect to the dashboard
+            navigate('/portal/dashboard');
+        } catch (error) {
+            setError('Invalid email or password');
+        }
+    };
+
     return (
-        <div class="row justify-content-center">
-            <div class="col-xl-10 col-lg-12 col-md-9">
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        {/* <!-- Nested Row within Card Body --> */}
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+        <div className="row justify-content-center">
+            <div className="col-xl-10 col-lg-12 col-md-9">
+                <div className="card o-hidden border-0 shadow-lg my-5">
+                    <div className="card-body p-0">
+                        <div className="row">
+                            <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div className="col-lg-6">
+                                <div className="p-5">
+                                    <div className="text-center">
+                                        <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
-                                    <form class="user">
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address..." />
+
+                                    {error && <p className="text-danger text-center">{error}</p>}
+
+                                    <form className="user" onSubmit={handleLogin}>
+                                        <div className="form-group">
+                                            <input
+                                                type="email"
+                                                className="form-control form-control-user"
+                                                id="exampleInputEmail"
+                                                placeholder="Enter Email Address..."
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                            />
                                         </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password" />
+                                        <div className="form-group">
+                                            <input
+                                                type="password"
+                                                className="form-control form-control-user"
+                                                id="exampleInputPassword"
+                                                placeholder="Password"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                            />
                                         </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck" />
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
-                                            </div>
-                                        </div>
-                                        <Link to="/portal/dashboard" class="btn btn-primary btn-user btn-block">
+                                        <button type="submit" className="btn btn-primary btn-user btn-block">
                                             Login
-                                        </Link>
-                                        <hr />
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
+                                        </button>
                                     </form>
+                                    
                                     <hr />
-                                    <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    <div className="text-center">
+                                        <Link className="small" to="/forgot-password">Forgot Password?</Link>
                                     </div>
-                                    <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
+                                    <div className="text-center">
+                                        <Link className="small" to="/register">Create an Account!</Link>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
-    )
+    );
 }
 
-export default Login
+export default Login;
